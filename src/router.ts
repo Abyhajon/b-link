d7c5bd49539d134d7cf121053846cc9cbc66743b
Abyhajon <joedabongo01@gmail.com>
2023-06-09 16:09:43 +0300

    created routes to translate and get translation

---
+++ src/router.ts
@@ -0,0 +1,31 @@
+import {Request, Response, Router} from 'express'
+import  { translateToBraille } from './server'
+
+const router = Router()
+
+//POST /translate
+router.post('/translate', (req, res) => {
+    const { text } = req.body;
+    if (!text) {
+        return res.status(400).json({ error: 'Text is required' })
+    }
+
+    const brailleTranslation = translateToBraille(text);
+
+    res.status(200).json({ brailleTranslation });
+});
+
+//GET /translation
+router.get('/translations', (req, res) => {
+    // Retrieve translations from the database or any other data source
+
+    // Dummy data for example purposes
+    const translations = [
+        { id: 1, text: 'Hello', brailleTranslation: '⠓⠑⠇⠇⠕' },
+        { id: 2, text: 'World', brailleTranslation: '⠺⠕⠗⠇⠙' },
+    ];
+
+    res.status(200).json({ translations })
+});
+
+export default router
